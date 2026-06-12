073a7d20c7fbe2bd2c20bbe6bab736afc5cc2f91
evertonrafaelsantos-create <everton.rafael.santos@escola.pr.gov.br>
2026-06-12 10:59:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+        "🌎 Pequenas ações fazem grandes diferenças. Economize água, recicle e plante árvores!";
+}
